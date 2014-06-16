9e5893e5adb8056977df9e04963774f6c1ec004a
Sean Dillingham <sean.dillingham@gmail.com>
2014-06-15 22:05:29 -0700

    add Bourbon and Slim gems, update config to activate Slim and set directories

--- config.rb
+++ config.rb
@@ -1,6 +1,14 @@
+require "slim"
+activate :bourbon
+activate :livereload
+
 ###
 # Compass
 ###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -24,9 +32,10 @@
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
 
 ###
 # Helpers
@@ -35,17 +44,17 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
 #     "Helping"
 #   end
 # end
+
+# Set slim-lang output style
+Slim::Engine.set_default_options :pretty => true
+
+set :partials_dir, 'partials'
 
 set :css_dir, 'stylesheets'
 
@@ -56,17 +65,22 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  # activate :cache_buster
 
   # Use relative URLs
   # activate :relative_assets
 
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # set :http_path, "/Content/images/"
 end
