d74436cf5c17690c78b6dcd50aba48c3114450a0
Jon White <mr.jon.white@gmail.com>
2014-06-17 11:56:22 -0700

    INSIDE KPCC dropdown now powered by JS, not CSS

--- source/javascripts/cosmetics.js
+++ source/javascripts/cosmetics.js
@@ -1,1 +1,8 @@
-//alert("hello there");
+//	================================================
+//	"Inside KPCC" dropdown reveal
+//	------------------------------------------------
+$(".titling .station > ul > li:nth-child(2)").mouseover(function(){
+    $(this).find("ul").show();
+}).mouseout(function(){
+    $(this).find("ul").hide();
+});
