4c36762caf96546b741583815522ca15bcedf409
Jon White <mr.jon.white@gmail.com>
2014-06-17 16:52:01 -0700

    Latest attempt to get dropdown reliably powered by JS

--- source/javascripts/cosmetics.js
+++ source/javascripts/cosmetics.js
@@ -1,8 +1,38 @@
-//	================================================
-//	"Inside KPCC" dropdown reveal
-//	------------------------------------------------
-$(".titling .station > ul > li:nth-child(2)").mouseover(function(){
-    $(this).find("ul").show();
-}).mouseout(function(){
-    $(this).find("ul").hide();
-});
+jQuery(document).ready(function($) {
+
+
+
+            //	================================================
+            //	Clicks or taps?
+            //	------------------------------------------------
+            function is_touch_device() {
+              return !!('ontouchstart' in window) // works on most browsers
+            	  || !!('onmsgesturechange' in window); // works on ie10
+            };
+
+
+
+
+            //	================================================
+            //	"Inside KPCC" dropdown reveal
+            //	------------------------------------------------
+            if(is_touch_device() == true) {
+
+                    $(".titling .station > ul > li:nth-child(2)").click(function(){
+                        $(this).find("ul").toggle;
+                    });
+
+            } else {
+
+                    $(".titling .station > ul > li:nth-child(2)").mouseover(function(){
+                        $(this).find("ul").show();
+                    }).mouseout(function(){
+                        $(this).find("ul").hide();
+                    });
+                    
+            }
+
+
+
+
+}); // end doc ready
